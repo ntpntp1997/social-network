e253f278b22913f5a83820a3bb80414eea1d9041
Phương nguyễn <phuong.n.t.97.08@gmail.com>
2019-11-07 12:14:08 +0700

    init readme.md

---
+++ README.md
@@ -0,0 +1,6 @@
+Author: Nguyễn Thanh Phương
+create project at : 4/11/2019
+facebook: https://www.facebook.com/nguyenthanhphuong.ntp
+
+this is a soccial-network in the lab and continues improves, this is my project for my exam in education!!!!
+Don't clone this for you. This is not OpenSource.
--- src/middleware/base/MiddlewaresBase.ts
+++ src/middleware/base/MiddlewaresBase.ts
@@ -3,25 +3,24 @@ import bodyParser = require("body-parser");
 import helmet from "helmet";
 import cors from "cors";
 import MethodOverride = require("./../MethodOverride");
-import BaseRoutes from './../../routes/base/BaseRoutes';
-import passPort from 'passport';
+import BaseRoutes from "./../../routes/base/BaseRoutes";
+import passPort from "passport";
 import PassPortInit = require("../../config/passport/passport");
 
 class MiddlewaresBase {
-    
-    static get configuration () {
-         var app = express();
-         app.use(passPort.initialize());
-         app.use(passPort.session());
-         app.use(cors());
-         app.use(helmet());
-         app.use(bodyParser.json());
-         app.use(bodyParser.urlencoded({ extended: true }));
-         app.use(MethodOverride.configuration());
-         new PassPortInit().initPassPortLocal()
-         app.use(new BaseRoutes().routes);
-         return app;
-    }    
+  static get configuration() {
+    var app = express();
+    app.use(passPort.initialize());
+    app.use(passPort.session());
+    app.use(cors());
+    app.use(helmet());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(MethodOverride.configuration());
+    new PassPortInit().initPassPortLocal();
+    app.use(new BaseRoutes().routes);
+    return app;
+  }
 }
 Object.seal(MiddlewaresBase);
 export = MiddlewaresBase;
